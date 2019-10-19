ac322284659586da32634ca06c034b21268aeeea
ykiu <32252655+ykiu@users.noreply.github.com>
2019-10-19 22:29:25 +0900

    Add API reference

--- doc/Main.js
+++ doc/Main.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import { Typography, Container, Link } from '@material-ui/core';
-// eslint-disable-next-line import/no-extraneous-dependencies
 import preval from 'preval.macro';
 import 'prismjs/themes/prism-okaidia.css';
 import { makeStyles } from '@material-ui/styles';
@@ -11,12 +10,91 @@ import Heading from './Heading';
 
 const SPACE = ' ';
 
+const PROPS = [
+  {
+    name: 'collapsedBackLayerHeight',
+    type: 'number',
+    default_: '56',
+    description: 'The height of the back layer when it is collapsed.',
+  },
+  {
+    name: 'expandedBackLayerHeight',
+    type: 'number',
+    default_: '300',
+    description: 'The height of the back layer when it is expanded.',
+  },
+  {
+    name: 'frontLayer',
+    type: 'node',
+    default_: '',
+    description: 'The contents of the front layer.',
+  },
+  { name: 'backLayer', type: 'node', default_: '', description: 'The contents of the back layer.' },
+  {
+    name: 'backLayerOpen',
+    type: 'bool',
+    default_: 'true',
+    description: 'Whether or not the back layer is open.',
+  },
+  {
+    name: 'onSwipeStart',
+    type: 'func',
+    default_: '',
+    description: 'Callback fired when a swipe starts.',
+  },
+  {
+    name: 'onTransitionEnd',
+    type: 'func',
+    default_: '',
+    description: 'Callback fired when a swipe and the subsequent animation ends.',
+  },
+  {
+    name: 'onBackLayerOpen',
+    type: 'func (required)',
+    default_: '',
+    description: 'Callback fired when the back layer requires to be opened.',
+  },
+  {
+    name: 'onBackLayerClose',
+    type: 'func (required),',
+    default_: '',
+    description: 'Callback fired when the back layer requires to be closed.',
+  },
+  {
+    name: 'classes',
+    type: 'object',
+    default_: '',
+    description: 'Override or extend the styles applied to the component.',
+  },
+];
+
 const useStyles = makeStyles(theme => ({
   demo: {
     marginBottom: theme.spacing(2),
   },
   code: {
     marginBottom: theme.spacing(2),
+  },
+  table: {
+    overflowX: 'auto',
+    width: '100%',
+    display: 'block',
+    borderSpacing: 0,
+    borderCollapse: 'collapse',
+    '& th,td': {
+      color: theme.palette.text.primary,
+      padding: theme.spacing(2),
+      fontSize: '14px',
+      whiteSpace: 'pre-wrap',
+      borderBottom: '1px solid rgba(0, 0, 0, 0.12)',
+    },
+    '& th': {
+      fontWeight: 500,
+      lineHeight: '1.5rem',
+    },
+    '& td': {
+      lineHeight: '1.43',
+    },
   },
 }));
 
@@ -62,7 +140,31 @@ function Main() {
       </ul>
       <Heading variant="h4">API</Heading>
       <Heading variant="h5">Backdrop</Heading>
-      <Typography gutterBottom>Props: ...</Typography>
+      <Heading variant="h6">Props</Heading>
+      <table className={classes.table}>
+        <thead>
+          <tr>
+            <th align="left">Name</th>
+            <th align="left">Type</th>
+            <th align="left">Default</th>
+            <th align="left">Description</th>
+          </tr>
+        </thead>
+        <tbody>
+          {PROPS.map(({ name, type, default_, description }) => (
+            <tr>
+              <td>
+                <code>{name}</code>
+              </td>
+              <td>
+                <code>{type}</code>
+              </td>
+              <td>{default_}</td>
+              <td>{description}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
       <Heading variant="h4">Why Material&quot;ish&quot;?</Heading>
       <Typography gutterBottom>
         This component is not strictly compliant with the Material Design specification because the
